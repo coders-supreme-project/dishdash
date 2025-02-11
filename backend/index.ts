e1d456ee3f084a3510f2fe56b58c4b911a1f15d9
sadok <sadokdronga@gmail.com>
2025-02-11 13:31:32 +0100

    prep repo

---
+++ backend/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+const app = express();
+
+app.use(express.json());
+
+app.get('/api/users', async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+});
+
+app.listen(3306, () => {
+  console.log('Backend server running on http://localhost:3001');
+});
