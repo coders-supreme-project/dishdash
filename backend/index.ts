4dcff4e92e3f2fdb2bcac1abdaa6be522d67ea90
Adem-Maafi <ademmaafi3@gmail.com>
2025-02-12 10:13:21 +0100

    inprogress customer

--- backend/index.ts
+++ backend/index.ts
@@ -1,16 +1,21 @@
 import express from 'express';
 import { PrismaClient } from '@prisma/client';
+import categorieRoutes from './router/categorie.routes';
+import reviewRoutes from './router/review.routes';
 
 const prisma = new PrismaClient();
 const app = express();
 
 app.use(express.json());
 
+
+app.use('/api', categorieRoutes);
+app.use('/api', reviewRoutes);
 app.get('/api/users', async (req, res) => {
   const users = await prisma.user.findMany();
   res.json(users);
 });
 
-app.listen(3306, () => {
+app.listen(3300, () => {
   console.log('Backend server running on http://localhost:3001');
 });
